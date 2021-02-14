1cc6a2f3489de76b64654eb39857bae9e8ef0b39
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-14 14:29:20 +0100

    make view index

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,16 @@
 class ListingsController < ApplicationController
 
+  def index
+  end
+
   def price_per_seller_type
 
   end
 
   def cars_by_make
+  end
+
+  def price_most_contacted
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'contacts#index'
+  root to: "listings#index"
 
   get "listings/price", to: "listings#price_per_seller_type"
   get "listings/cars", to: "listings#cars_by_make"
+  get "listings/price_contacted", to: "listings#price_most_contacted"
 
 end
