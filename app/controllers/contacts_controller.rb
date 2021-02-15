ab4d031ed3c8874a576cd00bbba2ef28497b62a0
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-15 23:38:06 +0100

    delete unnecessary method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,11 +9,4 @@ before_action :load_csv, only: [:contacts]
     Contact.import(params[:file])
     redirect_to listings_url
   end
-
-
-  def sort
-    @contacted = @contacts.reject{ |c| c[0] == "listing_id"}.map { |c| c[0]}
-    @counts = @contacted.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
-    @sorted = @counts.sort_by { |k,v| -v }
-  end
 end
