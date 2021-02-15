8e0bd8e1af00df94175e969a18f2b362e2014849
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-15 19:49:48 +0100

    delete unnecessary methods and data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,34 +1,23 @@
 class ContactsController < ApplicationController
 before_action :load_csv, only: [:contacts]
 
-def index
-  @contacts = Contact.all
-end
-
-def import
-  Contact.import(params[:file])
-  redirect_to contacts_url
-end
-
-
-
-def sort
-  @contacted = @contacts.reject{ |c| c[0] == "listing_id"}.map { |c| c[0]}
-  @counts = @contacted.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
-  @sorted = @counts.sort_by { |k,v| -v }
-end
-
-private
-
-  def load_csv
-    @contacts = []
-    @csv_file = "data/contacts.csv"
-    if File.exist?(@csv_file)
-      CSV.foreach(@csv_file) do |row|
-        @contacts << row
-      end
-    end
+  def index
+    @contacts = Contact.all
   end
+
+  def import
+    Contact.import(params[:file])
+    redirect_to contacts_url
+  end
+
+
+  def sort
+    @contacted = @contacts.reject{ |c| c[0] == "listing_id"}.map { |c| c[0]}
+    @counts = @contacted.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
+    @sorted = @counts.sort_by { |k,v| -v }
+  end
+
+
 end
 
 
