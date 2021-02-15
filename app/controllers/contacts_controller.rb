fc5f8dabe7191e7bce287d7721762a0080ab025f
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-15 23:59:02 +0100

    change routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,4 @@
 class ContactsController < ApplicationController
-before_action :load_csv, only: [:contacts]
-
   def index
     @contacts = Contact.all
   end
@@ -9,4 +7,31 @@ before_action :load_csv, only: [:contacts]
     Contact.import(params[:file])
     redirect_to listings_url
   end
+
+  def count
+    @contacts = Contact.group(:listing_id).order('count_listing_id DESC').count(:listing_id)
+  end
+
+  def price_most_contacted
+    @contacts = Contact.group(:listing_id).order('count_listing_id DESC').count(:listing_id)
+
+    @price_sum = 0
+    @contact_count = Contact.all.count * 0.3
+    @listing_count = 0
+
+
+    # loop do
+    #  break if @contact_count.zero?
+    #     @contacts.each do |contact|
+    #       @price_sum += Listing.find(contact[0]).price
+    #       @contact_count -= contact[1]
+    #       @listing_count += 1
+    #     end
+    # end
+  end
+
+  def price_top_five_contacted
+    @contacts = render contacts: 'count'
+  end
+
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -24,27 +24,4 @@ class ListingsController < ApplicationController
     @makes = Listing.group(:make).count.sort_by { |k,v| -v}
     @listings_count = Listing.all.count
   end
-
-  def price_most_contacted
-    @contacts = Contact.group(:listing_id).order('count_listing_id DESC').count(:listing_id)
-
-    @price_sum = 0
-    @contact_count = Contact.all.count * 0.3
-    @listing_count = 0
-
-
-    # loop do
-    #  break if @contact_count.zero?
-    #     @contacts.each do |contact|
-    #       @price_sum += Listing.find(contact[0]).price
-    #       @contact_count -= contact[1]
-    #       @listing_count += 1
-    #     end
-    # end
-
-  end
-
-  def price_top_five_contacted
-  end
-
 end
