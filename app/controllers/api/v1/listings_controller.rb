9aa46a1bdc0b4117816dcc945c79c7611c970f5a
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-17 17:24:34 +0100

    make api folders and controllers

---
+++ app/controllers/api/v1/listings_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ListingsController < Api::V1::BaseController
+  def index
+    @listings = Listing.all
+    @contacts = Contact.all
+  end
+
+  def price_per_seller_type
+    @privates = Listing.where(seller_type: "private")
+    @dealers = Listing.where(seller_type: "dealer")
+    @others = Listing.where(seller_type: "other")
+
+    @privates_price = @privates.sum(:price) / @privates.count
+    @dealers_price = @dealers.sum(:price) / @dealers.count
+    @others_price = @others.sum(:price) / @others.count
+  end
+
+  def cars_by_make
+    @makes = Listing.group(:make).count.sort_by { |k,v| -v}
+    @listings_count = Listing.all.count
+  end
+end
