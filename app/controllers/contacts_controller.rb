c1f83489703b0dee714f2ad5a0abcd2740589e8b
Chihoko Ueno <harrierraum@hotmail.com>
2021-02-17 19:07:07 +0100

    recover contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,66 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def import
+      Contact.delete_all unless Contact.count.zero?
+      Contact.import(params[:file])
+      redirect_to root_path
+    end
+
+  def price_most_contacted
+    @contacts = @contacts = Contact.group(:listing_id).order('count_listing_id DESC').count(:listing_id)
+    @price_sum = 0
+    @counter = 0
+    @contact_sum = 0
+
+      @contacts.each do |contact|
+        @counter += + 1
+        @price_sum += Listing.find(contact[0]).price
+        @contact_sum += contact[1]
+        if @contact_sum >= Contact.all.count * 0.3
+          @counter -= 1
+          @price_sum -= Listing.find(contact[0]).price
+          @contact_sum -= contact[1]
+          break
+        end
+      end
+    end
+
+  def price_top_five_contacted
+    @monthly = monthly_result
+    @result = {}
+    @top_five = {}
+
+    @monthly.each do |key ,value|
+      @result = value.sort_by { |k,v| -v}.first(5).to_h
+      if @top_five.has_key?(key)
+        @top_five[key] < @result
+      else
+        @top_five[key] = @result
+      end
+    end
+  end
+
+  def monthly_result
+    @contact_month = {}
+    @counts = {}
+
+    Contact.all.order(:contact_date).each do |cnt|
+      month = Time.at(cnt[:contact_date]/1000).strftime("%m.%Y")
+      if @contact_month.has_key?(month)
+        if @counts[cnt.listing_id.to_s]
+          @counts[cnt.listing_id.to_s] += 1
+        else
+          @counts[cnt.listing_id.to_s] = 1
+        end
+        @contact_month[month] < @counts
+      else
+        @counts[cnt.listing_id.to_s] = 1
+        @contact_month[month] = @counts
+      end
+    end
+    return @contact_month
+  end
+
 end
